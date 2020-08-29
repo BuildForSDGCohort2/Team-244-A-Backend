525485ac66d8f0eb52a2dcb2a2338bb83c9ed5d9
nadaabdelmaboud <42664649+nadaabdelmaboud@users.noreply.github.com>
2020-08-29 21:09:01 +0200

    routes-controllers-services-middlewares structure

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const logger = require("npmlog");
 const bodyParser = require("body-parser");
-const jsonwebtoken = require("jsonwebtoken");
-
 
 // create express app
 const app = express();
@@ -12,9 +10,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
 //routes
-
 
 // Configuring the database
 const dbConfig = require("./config/database.config.js");
@@ -23,32 +19,36 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 // Connecting to the database
-mongoose.connect(dbConfig.url, {
-	useNewUrlParser: true,useUnifiedTopology: true
-}).then(() => {
-  logger.info("Successfully connected to the database");    
-}).catch((err) => {
-  logger.error("Could not connect to the database. Exiting now...", err);
+mongoose
+  .connect(dbConfig.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    logger.info("Successfully connected to the database");
+  })
+  .catch((err) => {
+    logger.error("Could not connect to the database. Exiting now...", err);
     process.exit();
+  });
+
+// 404 handler
+app.use("*", (req, res, next) => {
+  next("API_NOT_FOUND", 404);
 });
 
-  // 404 handler
-  app.use("*", (req, res, next) => {
-    next("API_NOT_FOUND", 404);
-  });
-
-  // error handler
-  app.use((err, req, res, next) => {
-    logger.error("Error Message >", err.message);
-    if (!err.status) {
-        logger.error("Stack >", err.stack);
-      // console.error(err);
-      process.exit(0);
-    }
-    res.status(err.status).json({ message: err.message, status: err.status });
-  });
+// error handler
+app.use((err, req, res, next) => {
+  logger.error("Error Message >", err.message);
+  if (!err.status) {
+    logger.error("Stack >", err.stack);
+    // console.error(err);
+    process.exit(0);
+  }
+  res.status(err.status).json({ message: err.message, status: err.status });
+});
 
 // listen for requests
 app.listen(port, () => {
-    logger.info("Server is listening on port",port);
+  logger.info("Server is listening on port", port);
 });
